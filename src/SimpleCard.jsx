d546cf67684e76a05a6733b194d194d358825909
FREEDOM <freewynane62@gmail.com>
2021-06-09 19:43:09 +0000

    styling my app

--- src/SimpleCard.jsx
+++ src/SimpleCard.jsx
@@ -11,11 +11,12 @@ class SimpleCard extends Component {
         
         return (
             <div className="cardBody">
-                <div className="cardImage">
-                    <Image imgUrl={image}/>
-                </div>
+                
                 <header className="simple-card">
+                    <Image imgUrl={image}/>
+                
                     <Title title="Simple React Card"/>
+                    
                     <Description description="React is an open-source front-end JavaScript library for building user interfaces or UI components."/>
                 </header>
                 
